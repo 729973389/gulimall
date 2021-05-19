4ae777f84774d46cbd3c970ea137d0ba077080e7
729973389 <729973389@qq.com>
2021-05-19 14:25:56 +0800

    新建立的项目

---
+++ guilimall-product/src/test/java/com/study/gulimall/guilimallproduct/GuilimallProductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.gulimall.guilimallproduct;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuilimallProductApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
